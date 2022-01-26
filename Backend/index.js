2816bdfea41aa4d829a759f2e3d49fc4503f025e
aviv-raztkovski <12847141+aviv-raztkovski@users.noreply.github.com>
2022-01-26 12:27:00 +0200

    mongoDB setup

--- Backend/index.js
+++ Backend/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
-require('dotenv').config() ;
+require('dotenv').config();
+const db = require('./db');
 const routes = require('./routes');
 
 const app = express();
@@ -8,6 +9,7 @@ app.use(routes);
 
 const port = process.env.SERVER_PORT || 3000;
 
-app.listen(port, () => {
+app.listen(port, async () => {
+    await db.connect();
     console.log(`server open at http://localhost:${port}`);
 })
